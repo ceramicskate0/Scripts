21ac1988a9bc5351108e61bd020c1bde3dadaf7a
Ceramicskate0 <ceramicskate0@users.noreply.github.com>
2019-06-30 16:54:19 -0400

    Update Auto_Update_SYSMON.bat

--- WindowsBatch/Auto_Update_SYSMON.bat
+++ WindowsBatch/Auto_Update_SYSMON.bat
@@ -2,4 +2,5 @@
 cd C:\Windows
 @powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/ceramicskate0/sysmon-config/master/sysmonconfig-export.xml','C:\Windows\sysmonconfig-export.xml')"
 sysmon -c sysmonconfig-export.xml
+rm sysmonconfig-export.xml
 exit
