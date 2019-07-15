acfb0aef523427e3c98674d1026b77b496e85960
Ceramicskate0 <ceramicskate0@users.noreply.github.com>
2019-07-15 09:19:46 -0400

    Update Install_Sysmon.bat

--- WindowsBatch/Install_Sysmon.bat
+++ WindowsBatch/Install_Sysmon.bat
@@ -12,17 +12,17 @@ if "%hour:~0,1%"==" " set hour=0%hour:~1,1%
 if "%hour:~1,1%"=="" set hour=0%hour%
 if "%minute:~1,1%"=="" set minute=0%minute%
 set tasktime=%hour%:%minute%
-mkdir C:\ProgramData\sysmon
-pushd "C:\ProgramData\sysmon\"
+mkdir C:\Windows
+pushd "C:\Windows\"
 echo [+] Downloading Sysmon...
-@powershell (new-object System.Net.WebClient).DownloadFile('https://live.sysinternals.com/Sysmon.exe','C:\ProgramData\sysmon\sysmon.exe')"
+@powershell (new-object System.Net.WebClient).DownloadFile('https://live.sysinternals.com/Sysmon.exe','C:\Windows\sysmon.exe')"
 echo [+] Downloading Sysmon config...
-@powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/ceramicskate0/sysmon-config/master/sysmonconfig-export.xml','C:\ProgramData\sysmon\sysmonconfig-export.xml')"
-@powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/ceramicskate0/Scripts/master/WindowsBatch/Auto_Update_SYSMON.bat','C:\ProgramData\sysmon\Auto_Update_SYSMON.bat')"
+@powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/ceramicskate0/sysmon-config/master/sysmonconfig-export.xml','C:\Windows\sysmonconfig-export.xml')"
+@powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/ceramicskate0/Scripts/master/WindowsBatch/Auto_Update_SYSMON.bat','C:\Windows\Auto_Update_SYSMON.bat')"
 sysmon.exe -accepteula -i sysmonconfig-export.xml
 sc failure Sysmon actions= restart/10000/restart/10000// reset= 120
 echo [+] Sysmon Successfully Installed!
 echo [+] Creating Auto Update Task set to Hourly..
-SchTasks /Create /RU SYSTEM /RL HIGHEST /SC HOURLY /TN Update_Sysmon_Rules /TR C:\ProgramData\sysmon\Auto_Update_SYSMON.bat /F /ST %tasktime%
+SchTasks /Create /RU SYSTEM /RL HIGHEST /SC HOURLY /TN Update_Sysmon_Rules /TR C:\Windows\Auto_Update_SYSMON.bat /F /ST %tasktime%
 timeout /t 10
 exit
