a7b7bb56e1da2d73e02ed3f75e9e52ee62095164
Ceramicskate0 <ceramicskate0@users.noreply.github.com>
2020-01-22 13:12:15 -0800

    Create AddUserToGroup.cs

---
+++ cSharp/AddUserToGroup.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.DirectoryServices;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static string Name;
+        public static string Pass;
+        
+        static void Main(string[] args)
+        {
+            createUser("USER", "PW");
+        }
+
+        public static void createUser(string Name, string Pass)
+        {
+            try
+            {
+                DirectoryEntry AD = new DirectoryEntry("WinNT://" +
+                                    Environment.MachineName + ",computer");
+                DirectoryEntry NewUser = AD.Children.Add(Name, "user");
+                NewUser.Invoke("SetPassword", new object[] { Pass });
+                NewUser.Invoke("Put", new object[] { "Description", "Test User from .NET" });
+                NewUser.CommitChanges();
+                DirectoryEntry grp;
+
+                grp = AD.Children.Find("Administrators", "group");
+                if (grp != null) { grp.Invoke("Add", new object[] { NewUser.Path.ToString() }); }
+                Console.WriteLine("[*] Account Created Successfully");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("[!]"+ex.Message);
+            }
+
+        }
+    }
+}
